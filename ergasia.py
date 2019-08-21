db04ca8fc2dc76093edc2a8760179ea1df70e2e1
Dimitris Provatas <james.provatas@gmail.com>
2019-08-21 18:53:04 +0300

    1st iteration

---
+++ ergasia.py
@@ -0,0 +1,17 @@
+"""
+@author: Dimitris Provatas
+"""
+
+#Data mining
+
+#initialise strins with the file names to avoid confusion and save time
+test = 'test.arff'
+train = 'train.arff'
+
+#imports
+from scipy.io import arff
+from io import StringIO
+
+
+#reads the data
+data, meta= arff.loadarff(StringIO(test))
