86714c3e393d711efc817bcbabdcc50bf777c1c3
Dimitris Provatas <james.provatas@gmail.com>
2019-08-27 20:53:19 +0300

    Implementation of transformation from .arff files to .csv, added script to change non-numeric values in mostly numeric columns to numeric in order to normalize the data, not sure if I will use it

---
+++ change.py
@@ -0,0 +1,22 @@
+#change csv values
+import sys, getopt
+
+def changeValues(csv_path):
+    text = open(csv_path, "r")
+    text = ''.join([i for i in text]) \
+        .replace("5more", "5") \
+        .replace("more", "5")
+    x = open(csv_path,"w")
+    x.writelines(text)
+    x.close()
+
+if __name__ == '__main__':
+    
+    input_file = None
+    args, _ = getopt.getopt(sys.argv[1:], "i:o:")
+
+    if not input_file:
+        print("Usage: %s -i \"input_file.csv\"" % sys.argv[0])
+        exit(0)
+
+    changeValues(input_file)
